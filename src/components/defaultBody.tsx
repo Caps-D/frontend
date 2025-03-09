228420bf86ee5fdf03ebe1025ca0721acb157036
unknown <lily030624@gmail.com>
2025-03-10 04:09:25 +0900

    [feat] 헤더제작, 랭킹 화면 구현 중

---
+++ src/components/defaultBody.tsx
@@ -0,0 +1,23 @@
+
+
+interface DefaultBodyProps {
+    hasHeader : number; // 0:헤더 없음, 1:헤더 있음
+    children?: React.ReactNode;
+}
+
+const DefaultBody: React.FC<DefaultBodyProps> = ({ hasHeader, children }) => {
+    
+    
+    const pt = hasHeader? " pt-[108px]" : "";
+
+    return (
+       
+        <div 
+            id="scrollbar-hidden"
+            className={ "bg-white w-full flex flex-col px-[20px] mb-[110px] "+ pt }>
+            {children}
+        </div>
+    );
+};
+
+export default DefaultBody;
---
+++ src/components/header.tsx
@@ -0,0 +1,75 @@
+
+import React, { ReactNode } from "react";
+import BackButton from "./BackButton";
+import Title from "./Title";
+
+/** Header의 자식 요소 타입 */
+interface HeaderProps {
+    children: ReactNode;
+}
+
+/**
+ * 특정 컴포넌트(BackButton, Title 등)와 일치하는지 확인하는 헬퍼 함수
+ */
+function isElementType(child: ReactNode, component: any) {
+    return React.isValidElement(child) && child.type === component;
+}
+
+const Header = ({ children }: HeaderProps) => {
+    let backButton: ReactNode = null;
+    let title: ReactNode = null;
+    let searchButton: ReactNode = null;
+    let menu: ReactNode = null;
+    let logo: ReactNode = null;
+    let notice: ReactNode = null;
+    const others: ReactNode[] = [];
+
+    // children 순회하면서 원하는 컴포넌트를 찾아서 할당
+    React.Children.forEach(children, (child) => {
+        if (isElementType(child, Header.BackButton)) {
+            backButton = child;
+        } else if (isElementType(child, Header.Title)) {
+            title = child;
+        } else {
+            others.push(child);
+        }
+    });
+
+    return (
+        <header
+            className="
+                flex items-end justify-between
+                px-[20px] h-[80px] bg-white fixed top-0
+                left-1/2 -translate-x-1/2 right-0 z-10
+                w-full
+                max-w-[500px] 
+            "
+        >
+            {/* 왼쪽 영역: BackButton */}
+            <div className="flex items-center gap-2">
+                {backButton}
+                {logo}
+            </div>
+
+            {/* 중앙 영역: Title (항상 중앙 고정) */}
+            <div
+               className="flex font-[NeoDunggeunmo] items-baseline not-italic font-normal text-[26px] text-black ">
+                {title}
+            </div>
+
+            {/* 오른쪽 영역: SearchButton, Menu */}
+            <div className="flex items-center gap-2 pb-[23px]">
+                {searchButton}
+                {menu}
+                {notice}
+            </div>
+        </header>
+    );
+};
+
+// 확장 속성으로 각 컴포넌트를 연결
+Header.BackButton = BackButton;
+Header.Title = Title;
+
+
+export default Header;
