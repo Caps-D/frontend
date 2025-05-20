c1d4b152b431f16e2e08541d4c63641742710aa7
Yereong <162969032+yereong@users.noreply.github.com>
2025-05-20 13:33:05 +0900

    fix: 자잘한 에러 수정

--- src/page/start/start.tsx
+++ src/page/start/start.tsx
@@ -23,7 +23,7 @@ const Start = () => {
   // 운동 종류에 따라 서버 주소를 반환
   const getWebSocketUrl = () => {
     if (state.exerciseType === '스쿼트') {
-      return 'wss://h4capston.site/squat/wss';
+      return 'wss://h4capston.site/squat/ws';
     } else if (state.exerciseType === '팔굽혀펴기') {
       return 'wss://h4capston.site/pushup/ws';
     } else if (state.exerciseType === '플랭크') {
